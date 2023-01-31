82a3379bff2cd8a9e223eb7e6a8f0f3e255cdf99
Juti <jbuican19@gmail.com>
2023-01-31 19:56:07 +0000

    Update setup.php
    add set up file

--- wp-content/themes/demotheme/includes/setup.php
+++ wp-content/themes/demotheme/includes/setup.php
@@ -6,6 +6,19 @@ function d_setup_theme(){
     'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap',
     'assets/css/bootstrap/bootstrap.min.css',
     'assets/css/stylesheet.css',
-    'assets/js/overlay-menu/css/style12.css'
+    'assets/js/overlay-menu/css/style12.css',
+    'assets/css/flexslider/flexslider.css',
+    'assets/css/spinkit/spinkit.css',
+    'assets/js/magnific-popup/magnific-popup.css',
+    'assets/revolution/css/settings.css',
+    'assets/revolution/css/layers.css',
+    'assets/revolution/css/navigation.css',
+    'assets/revolution/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css',
+    'assets/revolution/fonts/font-awesome/css/font-awesome.min.css',
+    'assets/js/owl-carousel/owl.carousel.css',
+    'assets/js/owl-carousel/owl.theme.default.min.css',
+    'assets/fonts/simple-line-icons/css/simple-line-icons.css',
+    'assets/fonts/font-awesome/css/font-awesome.min.css'
+
   ]);
 }
